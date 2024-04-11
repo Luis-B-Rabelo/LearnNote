12afeb5d1702d1f96c3466df0650b0ee910d68b7
Luís Eduardo B. Rabelo <luiseduardobgrl@gmail.com>
2024-04-10 23:05:15 -0300

    Add project files.

---
+++ LearnNote/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LearnNote
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LearnNoteTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace LearnNoteTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
