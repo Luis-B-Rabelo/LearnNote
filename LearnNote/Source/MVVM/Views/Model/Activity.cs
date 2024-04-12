cee317f20a80140b8ea17ebffb083a85646b2200
Pedro Ávila <148865987+PedroAvilaD@users.noreply.github.com>
2024-04-12 11:03:46 -0300

    Adicionando os Models
    Adicionei as classes com seus atributos, porém ainda falta os métodos.

---
+++ LearnNote/Source/MVVM/Views/Model/Activity.cs
@@ -0,0 +1,21 @@
+﻿using Android.Text.Format;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnNote.Model
+{
+    public class Activity
+    {
+        private uint activityId;
+        private string name;
+        private string description;
+        private byte dayWeek;
+        private TimeOnly startTime;
+        private TimeOnly endTime;
+        private bool alarm;
+        private bool studyTime;
+    }
+}
---
+++ LearnNote/Source/MVVM/Views/Model/Notebook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnNote.Model
+{
+    public class Notebook
+    {
+        private uint notebookId;
+        private uint userId;
+        private string title;
+        private ushort quantityNotes;
+    }
+}
